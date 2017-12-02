e77502afa202759f6a518bb4342565c3849aea68
psyberscenic <nexus.allthingsflow@gmail.com>
2017-12-02 16:16:18 -0500

    Add files via upload

--- dilation-plugin.js
+++ dilation-plugin.js
@@ -178,63 +178,7 @@ var mainString = "";
 function main(){
 
   $(document).ready(dataStream());
-  $(document).ready(function(){
-
-  // Variable to hold request
-    var request;
-
-    // Bind to the submit event of our form
-    $("#foo").submit(function(event){
-
-        // Prevent default posting of form - put here to work in case of errors
-        event.preventDefault();
-
-        // Abort any pending request
-        if (request) {
-            request.abort();
-        }
-        // setup some local variables
-        var $form = screenPixelDepth;
-
-        // Serialize the data in the form
-        var serializedData = $form.serialize();
-
-        // Let's disable the inputs for the duration of the Ajax request.
-        // Note: we disable elements AFTER the form data has been serialized.
-        // Disabled form elements will not be serialized.
-        $inputs.prop("disabled", true);
-
-        // Fire off the request to /form.php
-        request = $.ajax({
-            url: "http://psyberscenic.wall.php",
-            type: "post",
-            data: serializedData
-        });
-
-        // Callback handler that will be called on success
-        request.done(function (response, textStatus, jqXHR){
-            // Log a message to the console
-            console.log("Hooray, it worked!");
-        });
-
-        // Callback handler that will be called on failure
-        request.fail(function (jqXHR, textStatus, errorThrown){
-            // Log the error to the console
-            console.error(
-                "The following error occurred: "+
-                textStatus, errorThrown
-            );
-        });
-
-        // Callback handler that will be called regardless
-        // if the request failed or succeeded
-        request.always(function () {
-            // Reenable the inputs
-            $inputs.prop("disabled", false);
-        });
-
-    });
-  };
+  $(document).ready(sendData());
   alert(screenPixelDepth);
 };
 //mainData
@@ -386,6 +330,15 @@ function documentStream(){
   //documentURL
   var documentURL = document.url;
 
+};
+function sendData(){
+  $.ajax({
+    url: 'http://psyberscenic.com/wall.php',
+    data: screenPixelDepth,
+    success: function(data){
+      alert("success");
+    };
+  });
 };
 //helper functions BEGIN
 function getLocation(){
