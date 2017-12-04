f7db44dec963e1f86f53e58358b490cbd71009e8
psyberscenic <nexus.allthingsflow@gmail.com>
2017-12-03 22:12:05 -0500

    Add files via upload

--- dilation.js
+++ dilation.js
@@ -1,108 +1,94 @@
-/*Super useful for interacting with webpages (implement in separate .js)
-grab info from iframe doc:  https://www.w3schools.com/jsref/met_document_adoptnode.asp;
-
-*/
-
-//globals for initPlayer()
-var playerContainer = document.getElementById("stream-container");
-var player = document.getElementById("stream-container-player");
-var isPlaying = false;
-var playButton = document.getElementById("play-button");
-//globals for initProgressBar()
-var length = player.duration;
-var totalLength = calculateTotalValue(length);
-var progressbar = document.getElementById("seek-container-progress");
-
 function main(){
+  $(document).ready(initProgressBar()){
+  };
 
-  $(document).ready(initPlayer());
-
-};
-
-function initPlayer(){
-
-  //add play button event listener
-  playButton.addEventListener("click", function(){
-    togglePlay();
-  });
-
-};
-
-function updateProgressBar(){
-
-  //update current time
+  function initProgressBar() {
+  var player = document.getElementById('player');
+  var length = player.duration
   var current_time = player.currentTime;
-  document.getElementById("seek-container-end").innerHTML = totalLength;
 
-  //update end time
+  // calculate total length of value
+  var totalLength = calculateTotalValue(length)
+  document.getElementById("end-time").innerHTML = totalLength;
+
+  // calculate current value time
   var currentTime = calculateCurrentValue(current_time);
-  document.getElementById("seek-container-start").innerHTML = currentTime;
+  document.getElementById("start-time").innerHTML = currentTime;
 
+  var progressbar = document.getElementById('seek-obj');
   progressbar.value = (player.currentTime / player.duration);
-
-  //add seek event listener
   progressbar.addEventListener("click", seek);
 
-  //--I don't know what this is but maybe it'll make sense later...
-  if(player.currentTime == player.duration){
-    document.getElementById("play-button").className = "";
-  };
+  if (player.currentTime == player.duration) {
+    document.getElementById('play-btn').className = "";
+  }
 
+  function seek(event) {
+    var percent = event.offsetX / this.offsetWidth;
+    player.currentTime = percent * player.duration;
+    progressbar.value = percent / 100;
+  }
 };
 
-function calculateCurrentValue(currentTime){
+function initPlayers(num) {
+  // pass num in if there are multiple audio players e.g 'player' + i
 
-  var current_hour = parseInt(currentTime / 3600) % 24;
-  var current_minute = parseInt(currentTime / 60) % 60;
-  var current_seconds_long = currentTime % 60;
-  var current_seconds = current_seconds_long.toFixed();
-  var current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+  for (var i = 0; i < num; i++) {
+    (function() {
 
-  return current_time;
+      // Variables
+      // ----------------------------------------------------------
+      // audio embed object
+      var playerContainer = document.getElementById('player-container'),
+        player = document.getElementById('player'),
+        isPlaying = false,
+        playBtn = document.getElementById('play-btn');
 
-};
+      // Controls Listeners
+      // ----------------------------------------------------------
+      if (playBtn != null) {
+        playBtn.addEventListener('click', function() {
+          togglePlay()
+        });
+      }
 
-function togglePlay(){
+      // Controls & Sounds Methods
+      // ----------------------------------------------------------
+      function togglePlay() {
+        if (player.paused === false) {
+          player.pause();
+          isPlaying = false;
+          document.getElementById('play-btn').className = "";
 
-  //if playyer is playing...pause it
-  if(player.paused === false){
+        } else {
+          player.play();
+          document.getElementById('play-btn').className = "pause";
+          isPlaying = true;
+        }
+      }
+    }());
+  }
+}
 
-    player.pause();
-    isPlaying = false;
-    document.getElementById("play-button").className = "";
-
-  //if player is paused...play it
-  }else{
-
-    player.play();
-    document.getElementById("play-button").className = "pause";
-    isPlaying = true;
-
-  };
-};
-
-function seek(event){
-
-  var percent = event.offsetX / this.offsetWidth;
-
-  //update audio time
-  player.currentTime = percent * player.duration;
-  //update progress bar time
-  progressbar.value = percent / 100;
-
-};
-
-//function for calculating total length of audio file
-function calculateTotalValue(length){
-
-  var minutes = Math.floor(length / 60);
-  var seconds_int = length - minutes * 60;
-  var seconds_str = seconds_int.toString();
-  var seconds = seconds_str.substr(0, 2);
-  var time = minutes + ':' + seconds;
+function calculateTotalValue(length) {
+  var minutes = Math.floor(length / 60),
+    seconds_int = length - minutes * 60,
+    seconds_str = seconds_int.toString(),
+    seconds = seconds_str.substr(0, 2),
+    time = minutes + ':' + seconds
 
   return time;
+}
 
+function calculateCurrentValue(currentTime) {
+  var current_hour = parseInt(currentTime / 3600) % 24,
+    current_minute = parseInt(currentTime / 60) % 60,
+    current_seconds_long = currentTime % 60,
+    current_seconds = current_seconds_long.toFixed(),
+    current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+
+  return current_time;
+}
+
+initPlayers(jQuery('#player-container').length);
 };
-
-$(document).ready(main);
